74ee7301d4a9616aab48a9f87ad28ec6df2b2c8f
GlobalMaximaNotLocalMaxima <user@user.com>
2022-12-06 20:25:11 -0500

    Added groundworks for the Songs Fragment

--- app/src/main/java/com/wishify/SongsFragment.java
+++ app/src/main/java/com/wishify/SongsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,8 @@ import android.view.ViewGroup;
 
 
 public class SongsFragment extends Fragment {
+    private RecyclerView recView;
+    private View view;
 
     public SongsFragment()
     {
@@ -20,6 +23,8 @@ public class SongsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState)
     {
+        this.view = view;
+        recView = view.findViewById(R.id.songs_rec_view);
 
     }
 }
