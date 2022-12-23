5ca567ccdd8f240ba2457998e61bc39de53d684d
GlobalMaximaNotLocalMaxima <user@user.com>
2022-12-23 18:59:39 -0500

    Fixed playlists being dumb v2

--- app/src/main/java/com/wishify/Globals.java
+++ app/src/main/java/com/wishify/Globals.java
@@ -157,6 +157,8 @@ public class Globals {
         {
             // Cleanup
             playlists.remove(playlistName);
+            PlaylistsFragment.adapter.refresh();
+            PlaylistsFragment.adapter.notifyDataSetChanged();
             return true;
         }
         else{
