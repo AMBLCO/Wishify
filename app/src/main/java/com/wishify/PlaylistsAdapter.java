e660e157fb767b2e73110a7a4f5625f1254d998b
GlobalMaximaNotLocalMaxima <user@user.com>
2022-12-23 16:20:48 -0500

    Continued playlists

--- app/src/main/java/com/wishify/PlaylistsAdapter.java
+++ app/src/main/java/com/wishify/PlaylistsAdapter.java
@@ -1,4 +1,110 @@
 package com.wishify;
 
-public class PlaylistsAdapter {
+import static com.wishify.Globals.addSongsToQueue;
+import static com.wishify.Globals.queue;
+import static com.wishify.Globals.queuePos;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class PlaylistsAdapter extends RecyclerView.Adapter<PlaylistsAdapter.ViewHolder>{
+
+    private static List<Playlist> playlistList = (List<Playlist>) Globals.getPlaylists().values();
+
+    public PlaylistsAdapter() {}
+
+    @NonNull
+    @Override
+    public PlaylistsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.rec_view_song_item, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.getPlaylistImageView().setImageBitmap(playlistList.get(position).getSongs().get(0).getBitmap());
+        holder.getPlaylistNameView().setText(playlistList.get(position).getSongs().get(0).getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return playlistList.size();
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView playlistImageView;
+        private TextView playlistNameView;
+
+        public ViewHolder(@NonNull View playlistView) {
+            super(playlistView);
+
+            playlistView.setOnClickListener(view -> {
+
+            });
+
+            playlistView.setOnLongClickListener(view ->
+            {
+                //Log.d("LONG_CLICK", "Registered long click");
+                PopupMenu popupMenu = new PopupMenu(view.getContext(), view, Gravity.END);
+                popupMenu.getMenuInflater().inflate(R.menu.playlists_popup_menu, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener( item -> {
+
+                    if (item.getItemId() == R.id.deletePlaylist)
+                    {
+                        // bye bye playlist
+
+                    }
+                    return false;
+                });
+
+                popupMenu.show();
+                return true;
+            });
+        }
+
+        public ImageView getPlaylistImageView() {
+            return playlistImageView;
+        }
+
+        public void setPlaylistImageView(ImageView playlistImageView) {
+            this.playlistImageView = playlistImageView;
+        }
+
+        public TextView getPlaylistNameView() {
+            return playlistNameView;
+        }
+
+        public void setPlaylistNameView(TextView playlistNameView) {
+            this.playlistNameView = playlistNameView;
+        }
+    }
+
+
+    public List<Playlist> getPlaylistList() {
+        return playlistList;
+    }
+
+    public void setPlaylistList(List<Playlist> playlistsList) {
+        playlistList = playlistsList;
+    }
 }
