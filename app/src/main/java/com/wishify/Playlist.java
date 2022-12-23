7cd722b58eff9976ea6c4c7100ab781fbddb5b47
GlobalMaximaNotLocalMaxima <user@user.com>
2022-12-23 17:01:57 -0500

    Playlist continued 4

--- app/src/main/java/com/wishify/Playlist.java
+++ app/src/main/java/com/wishify/Playlist.java
@@ -1,5 +1,10 @@
 package com.wishify;
 
+import android.content.Context;
+import android.util.Log;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 import java.util.List;
 
 public class Playlist {
@@ -17,7 +22,35 @@ public class Playlist {
         return this.songs;
     }
 
-    public void addSong(Song song){ songs.add(song); }
+    public int addSong(Song song){
+
+        try {
+            // Create file
+            writeSongToPlaylistFile(song);
+            songs.add(song);
+            PlaylistsFragment.adapter.notifyDataSetChanged();
+            return 1;
+        }
+        catch(Exception e)
+        {
+            Log.e("PLAYLIST_ADD", e.toString());
+            return 0; // FAILURE
+        }
+    }
+
+    private void writeSongToPlaylistFile(Song song) {
+
+        try(BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(Globals.getAppContext().openFileOutput(name + ".txt", Context.MODE_APPEND))))
+        {
+            writer.write(song.getUri().toString());
+            writer.newLine();
+            Log.d("PLAYLIST_ADD", "Added song to playlist");
+        }
+        catch(Exception e)
+        {
+            Log.e("PLAYLIST_ADD", "Could not add song to playlist!");
+        }
+    }
 
     public String getName() {
         return this.name;
